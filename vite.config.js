6bdf0c627bd2689ee636d941048c8770e0d062de
kmkkiii <rskkmkmk@gmail.com>
2023-01-09 19:28:29 +0900

    デプロイ設定変更

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  base: 'water-divination'
+  base: process.env.GITHUB_PAGES ? 'water-divination'  : './',
 })
